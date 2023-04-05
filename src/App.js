31444e8418e467550730fd746097aeed063ed4fe
kamilaut <kamila.utyukova@gmail.com>
2023-04-05 20:38:24 +0200

    Show a List of Suggestions During Search

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import EventList from './EventList';
+import CitySearch from './CitySearch';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <CitySearch />
 <EventList />
       </div>
     );
