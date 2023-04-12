b71805d358035c374f92ddd481cd300151fdc8e3
kamilaut <kamila.utyukova@gmail.com>
2023-04-12 18:11:15 +0200

    correcting mistakes

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
 import CitySearch from '../CitySearch';
+import NumberOfEvents from '../NumberOfEvents';
 
 
 describe('<App /> component', () => {
@@ -16,14 +17,11 @@ describe('<App /> component', () => {
     });
     
     test('render CitySearch', () => {
-      const AppWrapper = shallow(<App />);
       expect(AppWrapper.find(CitySearch)).toHaveLength(1);
     });
-    test('pass NumberOfEvents state to', () => {
-      const AppWrapper = mount(<App />);
-      const AppNumberOfEventsState = AppWrapper.state('numberOfEvents');
-      expect(AppNumberOfEventsState).not.toEqual(undefined);
-      expect(AppWrapper.find(NumberOfEvents).props().numberOfEvents).toEqual(35);
-      AppWrapper.unmount();
+    
+    test('render NumberOfEvents correctly', () => {
+      expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
     });
+  
   });
