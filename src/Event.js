cf97222d8d11313939d473c1a8907bb4fc3f15d7
kamilaut <kamila.utyukova@gmail.com>
2023-06-12 17:24:58 +0200

    creating event component
    showing/hiding event details

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,41 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Event extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDetails: false, 
+    };
+  }
+
+  toggleDetails = () => {
+    this.setState((prevState) => ({
+      showDetails: !prevState.showDetails,
+    }));
+  };
+
   render() {
-    return <div></div>;
+    const { event } = this.props;
+    const { showDetails } = this.state;
+
+    return (
+      <div className="event">
+        <h1 className="event-title">{event.summary}</h1>
+        <p className="event-location">{event.location}</p>
+        <p className="event-date-time">{event.start.dateTime}</p>
+
+        {showDetails && (
+          <div className="event-details">
+            <p className="event-description">{event.description}</p>
+          </div>
+        )}
+
+        <button className="toggle-details-btn" onClick={this.toggleDetails}>
+          {showDetails ? 'Hide Details' : 'Show Details'}
+        </button>
+      </div>
+    );
   }
 }
+
 export default Event;
