c2646f23b7d52e600de3d657855a36f8064665ef
kamilaut <kamila.utyukova@gmail.com>
2023-06-23 12:32:59 +0200

    User Can Select a City from the List of Suggestions

--- src/App.js
+++ src/App.js
@@ -4,15 +4,47 @@ import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import Event from './Event';
+import { getEvents, extractLocations } from './api';
 
 
 class App extends Component {
+  _isMounted = false;
+  
+  state = {
+    events: [],
+    locations: []
+  }
+  
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount(){
+    this.mounted = false;
+  }
+  
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = (location === 'all') ?
+        events :
+        events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      });
+    });
+  }
+  
+  
   render() {
     return (
       <div className="App">
-        <CitySearch />
-        <EventList />
-        <NumberOfEvents />
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+        <EventList events={this.state.events} />
       </div>
     );
   }
--- src/CitySearch.js
+++ src/CitySearch.js
@@ -24,20 +24,28 @@ class CitySearch extends Component {
             this.setState({
               query: suggestion,
             });
-          }
+            
+          this.props.updateEvents(suggestion);
+        }
     
   render() {
     return (
         <div className="CitySearch">
-           <input type="text" className="city" value={this.state.query} onChange={this.handleInputChanged} />
+           <input type="text" 
+           className="city" 
+           value={this.state.query} 
+           onChange={this.handleInputChanged} 
+           />
         <ul className="suggestions">
         {this.state.suggestions.map((suggestion) => (
     <li
     key={suggestion}
     onClick={() => this.handleItemClicked(suggestion)}
-  >{suggestion}</li>
+  >
+    {suggestion}
+    </li>
 ))}
-<li>
+ <li onClick={() => this.handleItemClicked("all")}>
   <b>See all cities</b>
 </li>
         </ul>
