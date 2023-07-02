2441cdbc127e8be38e94ac4b3e9598723e8b3db1
kamilaut <kamila.utyukova@gmail.com>
2023-07-02 12:01:27 +0200

    adding events to the mock data

--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -1,6 +1,7 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
 import React from 'react';
 import { mount } from 'enzyme';
+import { act } from 'react-dom/test-utils'; 
 import App from '../App';
 
 const feature = loadFeature('./src/features/specifyNumberOfEvents.feature');
@@ -16,29 +17,32 @@ defineFeature(feature, (test) => {
     AppWrapper.unmount();
   });
 
-  test('When user hasn\'t specified a number, 32 is the default number', ({ given, when, then }) => {
+  test('When user hasn\'t specified a number, 32 is the default number', async ({ given, when, then }) => {
     given('that the user has opened the events page', () => {
     });
 
     when('the user has not specified the number of events to be displayed', () => {
     });
 
-    then('the default number of events displayed should be 32', () => {
-      AppWrapper.update();
-      expect(AppWrapper.find('.event')).toHaveLength(32);
+    then('the default number of events displayed should be 32', async () => {
+      await act(async () => {
+        AppWrapper.update();
+        await new Promise((resolve) => setTimeout(resolve, 0)); // delay to allow the component to update
+        expect(AppWrapper.find('.event')).toHaveLength(32);
+      });
     });
   });
-
+  
   test('User can change the number of events they want to see', ({ given, when, then }) => {
     given('that the user has opened the events page', () => {
     });
 
     when('the user selects the option to change the number of events displayed', () => {
-      AppWrapper.find('.number-of-events').simulate('change', { target: { value: 10 } });
+      AppWrapper.find('.number-of-events-input').simulate('change', { target: { value: 10 } });
     });
 
-    then('the page should update to display the selected number of events', () => {
-      AppWrapper.update();
+    then('the page should update to display the selected number of events', async () => {
+      await AppWrapper.update();
       expect(AppWrapper.find('.event')).toHaveLength(10);
     });
 
@@ -48,3 +52,4 @@ defineFeature(feature, (test) => {
     });
   });
 });
+
